3c0937c9b39788a99c5fae83adac8ec62d433941
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2022-11-13 21:43:05 +0530

    export default in es moduling pattern

---
+++ default_case.js
@@ -0,0 +1,15 @@
+function fun1(){
+    console.log("this is fun1");
+}
+
+function gun1() {
+    console.log("this is gun1");
+}
+export function hun1(){
+    console.log("this is hun1");
+}
+
+export default{
+    fun1,
+    gun1
+}
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import {printEmployeeName, printEmployeeAddress} from "./employee.js";
 import {printingStudentName, printStudentAddress} from "./student.js";
+import default_example, {hun1} from "./default_case.js";
 
 printEmployeeName("Harshit Raj");
 printEmployeeAddress("West of housing colony, Chapra, Bihar");
@@ -7,3 +8,9 @@ printEmployeeAddress("West of housing colony, Chapra, Bihar");
 
 printingStudentName("Manikesh Ranjan");
 printStudentAddress("Inderpuri, Bhopal");
+
+
+console.log(default_example);
+default_example.fun1();
+default_example.gun1();
+hun1();
